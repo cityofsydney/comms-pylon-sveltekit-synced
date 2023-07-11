b3f2d785439a0248d9c64836e0c83b7f8c5a929b
dskelton-cos <dskelton@cityofsydney.nsw.gov.au>
2023-07-11 14:04:44 +1000

    2 files modified, 1 added

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,9 +1,11 @@
 import type { LayoutServerLoad } from './$types';
+import {addQRcode} from '$lib/qrcode';
 
 async function fetchEvents(param: String) {
 	const response = await fetch(`https://cos-express-api.web.app/whatson/algolia-events/` + param);
-	const data = await response.json();
-	return data;
+	const res = await response.json();
+	//const data = await addQRcode(res);
+	return res;
 }
 
 export const load = (async () => {
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,27 +3,7 @@
 import { generateSlides } from '$lib';
 import { Splide, SplideSlide } from '@splidejs/svelte-splide';
 import '@splidejs/svelte-splide/css';
-//import '@splidejs/svelte-splide/css/sea-green';
 import { onMount } from 'svelte';
-
-import QRCode from 'qrcode';
-
-
-const generateQR = slug => {
-  let whatsOnUrl =
-          `https://whatson.cityofsydney.nsw.gov.au/events/${slug}?utm_medium=outdoor&utm_source=qms-touch&utm_campaign=whatson`;
-
-  // With promises
-  QRCode.toDataURL('I am a pony!')
-    .then(url => {
-      console.log(url)
-        return `<img src="${url}" alt loading="lazy" />` 
-    })
-    .catch(err => {
-      console.error(err)
-    })
-
-}
 
 // Retrieve user store from context
 const nowShowing = getContext('nowShowing');
@@ -86,7 +66,7 @@ onMount(() => {
           <div class="absolute top-0 p-12 w-full flex flex-col">
             <h1 class="text-white font-heading text-6xl leading-tight mb-5">{slide.name}</h1>
             ---
-           <img src="{QRCode.toDataURL('I am a pony!', function (err, url) {return url})}" />
+           <img src="" />
             ---
 
           </div>
