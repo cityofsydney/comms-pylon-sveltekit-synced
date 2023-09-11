689b4063ca5f3f92f44430bde343779316847f8f
David Skelton <dskelton@cityofsydney.nsw.gov.au>
2023-09-11 16:04:14 +1000

    wokring realtime

--- src/routes/(emergency)/emergency/+page.server.ts
+++ src/routes/(emergency)/emergency/+page.server.ts
@@ -1,42 +1,9 @@
-import { CITYSWITCH_DATOCMS_API_TOKEN } from '$env/static/private';
+
 import type { PageServerLoad } from './$types';
-import { subscribeToQuery } from 'datocms-listen';
 
 export const load: PageServerLoad = async ({ params }) => {
 
-    const unsubscribe = await subscribeToQuery({
-        query: `query MyQuery {
-            allPages {
-                id
-                title
-
-            }
-        }`,
-        variables: { first: 10 },
-        token: CITYSWITCH_DATOCMS_API_TOKEN,
-        preview: true,
-        onUpdate: (update) => {
-            // response is the GraphQL response
-            console.log(update.response.data);
-
-            
-        },
-        onStatusChange: (status) => {
-            // status can be "connected", "connecting" or "closed"
-            //console.log(status);
-        },
-        onChannelError: (error) => {
-            console.error(error);
-        },
-        onError: (error) => {
-            console.log(error.message);
-        },
-        onEvent: (event) => {
-            //console.log('event: ',event)
-        }
-    });
-
     return {
-        post: unsubscribe,
+        post: 'dad',
     };
 };
--- src/routes/(emergency)/emergency/+page.svelte
+++ src/routes/(emergency)/emergency/+page.svelte
@@ -1,23 +1,67 @@
 <script lang="ts">
-
-
 	// SvelteKit Imports
 	import { onMount } from 'svelte';
 	import { browser } from '$app/environment';
+
+	import { PUBLIC_CITYSWITCH_DATOCMS_API_TOKEN } from '$env/static/public';
+	import { subscribeToQuery } from 'datocms-listen';
 
 	// +page.ts
 	import type { PageData } from './$types';
 	export let data: PageData;
 
+	// Stores
+	import { emergencyData, emergencyStatus } from '$lib/stores';
 
+	let promise = getRandomNumber();
 
-	// When DOM mounted, scroll to bottom
+	async function getRandomNumber() {
+		const unsubscribe = await subscribeToQuery({
+			query: `query MyQuery {
+				allPages {
+					id
+					title
+				}
+			}`,
+			variables: { first: 10 },
+			token: PUBLIC_CITYSWITCH_DATOCMS_API_TOKEN,
+			preview: true,
+			onUpdate: (update) => {
+				// response is the GraphQL response
+				//console.log(update.response.data);
+				const data = update.response.data;
+				emergencyData.set(data.allPages);
+			},
+			onStatusChange: (status) => {
+				// status can be "connected", "connecting" or "closed"
+				console.log(status);
+				emergencyStatus.set(status);
+			},
+			onChannelError: (error) => {
+				console.error(error);
+			},
+			onError: (error) => {
+				console.log(error.message);
+			},
+			onEvent: (event) => {
+				//console.log('event: ',event)
+			}
+		});
+
+		return unsubscribe;
+	}
+
 	onMount(() => {
 		console.log('Mounted');
-
 	});
-
 </script>
 
-{data.post}
-
+{#await promise}
+	<p>... {$emergencyStatus}</p>
+{:then data}
+	{#each $emergencyData as item}
+		{item.title}
+	{/each}
+{:catch error}
+	<p style="color: red">{error.message}</p>
+{/await}
