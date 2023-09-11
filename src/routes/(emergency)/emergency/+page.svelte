45d104d68f6e340a45adc0102778e7491e2f3321
dskelton-cos <dskelton@cityofsydney.nsw.gov.au>
2023-09-11 15:13:10 +1000

    breoken, but code

--- src/routes/(emergency)/emergency/+page.svelte
+++ src/routes/(emergency)/emergency/+page.svelte
@@ -1,1 +1,23 @@
-hello
+<script lang="ts">
+
+
+	// SvelteKit Imports
+	import { onMount } from 'svelte';
+	import { browser } from '$app/environment';
+
+	// +page.ts
+	import type { PageData } from './$types';
+	export let data: PageData;
+
+
+
+	// When DOM mounted, scroll to bottom
+	onMount(() => {
+		console.log('Mounted');
+
+	});
+
+</script>
+
+{data.post}
+
