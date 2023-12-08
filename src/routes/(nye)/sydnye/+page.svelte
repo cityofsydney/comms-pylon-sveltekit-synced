d38ed52513a0e6744f5c6afce8f108339bbc621d
David Skelton <dskelton@cityofsydney.nsw.gov.au>
2023-12-08 12:04:39 +1100

    styling

--- src/routes/(nye)/sydnye/+page.svelte
+++ src/routes/(nye)/sydnye/+page.svelte
@@ -16,8 +16,8 @@
 	});
 </script>
 
-<div class="flex flex-col bg-[url('/img/sydnye-bg.png')] bg-cover h-full pl-48 pr-40 pt-52 pb-72">
-	<div class="bg-blue-200/10 flex flex-col max-h-full h-full">
+<div class="flex flex-col bg-origin-border bg-left-top bg-[url('/img/sydnye-bg.png')] bg-no-repeat bg-cover h-full pl-48 pr-40 pt-52 pb-72">
+	<div class="flex flex-col max-h-full h-full">
 		<h1 class="text-8xl leading-tight">Vantage point live updates</h1>
 
 		<div class="grid grid-cols-2 mt-24 text-4xl gap-6">
@@ -37,7 +37,7 @@
 				>
 			</div>
 
-			<div class="basis-3/4">Barangaroo Reserve</div>
+			<div class="basis-3/4">Bradfield Park & Mary Booth Reserve</div>
 			<div class="basis-1/4 ml-auto text-right flex items-center">
 				Open
 				<svg
@@ -53,15 +53,15 @@
 				>
 			</div>
 
-			<div class="basis-3/4">Barangaroo Reserve</div>
+			<div class="basis-3/4">Royal Botanic Garden Sydney: Bennelong Lawn</div>
 			<div class="basis-1/4 ml-auto text-right flex items-center">
-				Open
+				Allmost full
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					width="32"
 					height="32"
 					viewBox="0 0 24 24"
-					class=" text-green-500 ml-8 w-10 h-10"
+					class=" text-orange-500 ml-8 w-10 h-10"
 					><path
 						fill="currentColor"
 						d="M12 2A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2Z"
@@ -71,13 +71,13 @@
 
 			<div class="basis-3/4">Barangaroo Reserve</div>
 			<div class="basis-1/4 ml-auto text-right flex items-center">
-				Open
+				Full
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					width="32"
 					height="32"
 					viewBox="0 0 24 24"
-					class=" text-green-500 ml-8 w-10 h-10"
+					class=" text-red-800 ml-8 w-10 h-10"
 					><path
 						fill="currentColor"
 						d="M12 2A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2Z"
@@ -86,4 +86,40 @@
 			</div>
 		</div>
 	</div>
+</div>
+
+<div class="sr-only">
+	<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="32"
+					height="32"
+					viewBox="0 0 24 24"
+					class=" text-green-500 ml-8 w-10 h-10"
+					><path
+						fill="currentColor"
+						d="M12 2A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2Z"
+					/></svg
+				>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="32"
+					height="32"
+					viewBox="0 0 24 24"
+					class=" text-orange-500 ml-8 w-10 h-10"
+					><path
+						fill="currentColor"
+						d="M12 2A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2Z"
+					/></svg
+				>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="32"
+					height="32"
+					viewBox="0 0 24 24"
+					class=" text-red-800 ml-8 w-10 h-10"
+					><path
+						fill="currentColor"
+						d="M12 2A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2Z"
+					/></svg
+				>
 </div>
