fd1ff4b1eec6a8b7a06312e00247f840072c88f7
David Skelton <dskelton@cityofsydney.nsw.gov.au>
2023-12-08 14:38:41 +1100

    add error page

---
+++ src/routes/(nye)/sydnye/+error.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div
+	class="flex flex-col bg-origin-border bg-left-top bg-[url('/img/sydnye-bg.png')] bg-no-repeat bg-cover h-full pl-48 pr-44 pt-52 pb-72"
+>
+	<div class="flex flex-col max-h-full h-full">
+		<h1 class="text-8xl leading-tight mb-24 font-heading-token">Vantage point live updates</h1>
+
+		<!-- <h1>{$page.status}: {$page.error?.message}</h1> -->
+
+        <p class="text-6xl leading-tight mb-24">
+            Visit the official Sydney New Year’s Eve website for public transport information
+        </p>
+
+        <p class="text-6xl leading-tight font-heading-token">sydneynewyearseve.com</p>
+
+
+
+	</div>
+</div>
--- src/routes/(nye)/sydnye/+page.svelte
+++ src/routes/(nye)/sydnye/+page.svelte
@@ -1,25 +1,22 @@
 <script lang="ts">
 	// SvelteKit Imports
-	import { onMount } from 'svelte';
-	// Stores
-	//import { emergencyData, emergencyStatus } from '$lib/stores';
+	//import { onMount } from 'svelte';
 
-	// +page.ts
 	import type { PageData } from './$types';
 	export let data: PageData;
 	$: ({ items } = data);
 
-	$: console.log(items)
-	onMount(() => {
+	//$: console.log(items)
+	/* onMount(() => {
 		console.log('Mounted');
-	});
+	}); */
 </script>
 
 <div
 	class="flex flex-col bg-origin-border bg-left-top bg-[url('/img/sydnye-bg.png')] bg-no-repeat bg-cover h-full pl-48 pr-44 pt-52 pb-72"
 >
 	<div class="flex flex-col max-h-full h-full">
-		<h1 class="text-8xl leading-tight">Vantage point live updates</h1>
+		<h1 class="text-8xl leading-tight font-heading-token">Vantage point live updates</h1>
 
 		<div class="grid grid-cols-2 w-full mt-24 text-4xl items-start">
 			{#each items as item}
@@ -36,7 +33,7 @@
 						width="32"
 						height="32"
 						viewBox="0 0 24 24"
-						class="text-green-500 ml-8 w-10 h-10 {item.statusAlert === 'Almost full'
+						class="ml-8 w-10 h-10 {item.statusAlert === 'Almost full'
 							? 'text-orange-500'
 							: item.statusAlert === 'Full'
 							? 'text-red-700'
@@ -52,38 +49,5 @@
 	</div>
 </div>
 
-<div class="sr-only">
-	<svg
-		xmlns="http://www.w3.org/2000/svg"
-		width="32"
-		height="32"
-		viewBox="0 0 24 24"
-		class=" text-green-500 ml-8 w-10 h-10"
-		><path
-			fill="currentColor"
-			d="M12 2A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2Z"
-		/></svg
-	>
-	<svg
-		xmlns="http://www.w3.org/2000/svg"
-		width="32"
-		height="32"
-		viewBox="0 0 24 24"
-		class=" text-orange-500 ml-8 w-10 h-10"
-		><path
-			fill="currentColor"
-			d="M12 2A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2Z"
-		/></svg
-	>
-	<svg
-		xmlns="http://www.w3.org/2000/svg"
-		width="32"
-		height="32"
-		viewBox="0 0 24 24"
-		class=" text-red-800 ml-8 w-10 h-10"
-		><path
-			fill="currentColor"
-			d="M12 2A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2Z"
-		/></svg
-	>
+<div class="sr-only text-green-500 text-orange-500 text-red-700">
 </div>
