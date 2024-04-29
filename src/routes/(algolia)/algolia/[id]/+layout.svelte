24c616abfd0793eb3db7c90510025be9e432a8b7
David Skelton <dskelton@cityofsydney.nsw.gov.au>
2024-04-29 14:05:24 +1000

    add some style

--- src/routes/(algolia)/algolia/[id]/+layout.svelte
+++ src/routes/(algolia)/algolia/[id]/+layout.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
+	// The ordering of these imports is critical to your app working properly
+	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+	import '../algolia.postcss';
 
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 </script>
 
 <slot />
--- src/routes/(algolia)/algolia/[id]/+page.server.ts
+++ src/routes/(algolia)/algolia/[id]/+page.server.ts
@@ -3,8 +3,8 @@ import type { PageServerLoad } from './$types';
 export const load = (async  ({ params }) => {
 	return {
 		post: {
-			title: `Title for ${params.id} goes here`,
-			content: `Content for ${params.id} goes here`,
+			title: `Title for ${params.id}`,
+			content: `Content for ${params.id}`,
 		},
 	};
-}) satisfies PageServerLoad;;
+}) satisfies PageServerLoad;
